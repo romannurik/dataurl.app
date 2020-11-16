3a5b982c610c3f0fbe70356b899f2ce78456435f
Roman Nurik <roman@nurik.net>
2020-11-16 12:42:26 -0500

    Initial code drop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import cn from 'classnames';
+import React from "react";
+import "./App.scss";
+import { FileDropTarget } from "./components/file-drop-target/file-drop-target";
 
-function App() {
+export default function App() {
+  let outRef = React.useRef();
+  let [out, setOut] = React.useState(null);
+  let [isImage, setIsImage] = React.useState(false);
+
+  let processText = str => {
+    let type = 'text/plain';
+    let isImage = false;
+    if (str.match(/<html/)) {
+      type = 'text/html';
+    } else if (str.match(/<svg/)) {
+      type = 'image/svg+xml';
+      isImage = true;
+    }
+    setOut(`data:${type};base64,${btoa(str)}`);
+    setIsImage(isImage);
+  };
+
+  let processFile = file => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      setOut(reader.result);
+      setIsImage(file.type.match(/image\//));
+    }, false);
+
+    reader.readAsDataURL(file);
+  };
+
+  React.useEffect(() => {
+    if (out && outRef.current) {
+      selectNode(outRef.current);
+    }
+  }, [out]);
+
+  React.useEffect(() => {
+    let pasteHandler = event => {
+      let clipboardData = event.clipboardData;
+      if (clipboardData.files && clipboardData.files.length) {
+        // TODO: check for png/gif?
+        processFile(clipboardData.files[0]);
+      } else {
+        for (let item of Array.from(clipboardData.items).filter(({type}) => type == 'text/plain')) {
+          item.getAsString(str => processText(str));
+          break;
+        }
+      }
+    };
+
+    window.addEventListener("paste", pasteHandler, false);
+    return () => window.removeEventListener("paste", this._pasteHandler, false);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FileDropTarget
+        className={cn('main', {
+          'is-zero': !out,
+          'is-image': isImage,
+        })}
+        regex={/\.(png|webp|jpg|svg|txt)$/}
+        invalidFileMessage="Bad file"
+        onDrop={file => processFile(file)}>
+      {out && <>
+        <div className="output"
+          onClick={ () => selectNode(outRef.current) }>
+          <div className="output-main">
+            <h2>Here's your Data URL!</h2>
+            <div className="output-data-url" ref={outRef}>{out}</div>
+          </div>
+        </div>
+      </>}
+      {out && isImage && <div className="preview-image">
+        <img src={out} />
+      </div>}
+      <div className='dropzone'>
+        {out && <>
+          <h1>Drop, paste or select another file</h1>
+        </>}
+        {!out && <div className="copy">
+          <h1>Drop, paste or select a file</h1>
+          <p>
+            You'll get a Data URL with the file's contents
+            to use in an <code>&lt;img&gt;</code> tag, a CSS <code>url(...)</code>,
+            etc.
+          </p>
+        </div>}
+        <input type="file"
+          onChange={ evt => {
+            processFile(evt.target.files[0]);
+            evt.target.value = null;
+          }} />
+      </div>
+    </FileDropTarget>
   );
 }
 
-export default App;
+function selectNode(node) {
+  if (document.selection) {
+    let range = document.body.createTextRange();
+    range.moveToElementText(node);
+    range.select();
+  } else if (window.getSelection) {
+    let range = document.createRange();
+    range.selectNode(node);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+  }
+}
---
+++ src/components/file-drop-target/file-drop-target.js
@@ -0,0 +1,77 @@
+import cn from "classnames";
+import React from "react";
+import "./file-drop-target.scss";
+
+export class FileDropTarget extends React.Component {
+  state = {
+    isActive: false
+  };
+
+  onDragEnter = ev => {
+    if (this.props.disabled) {
+      return;
+    }
+
+    ev.preventDefault();
+    if (this.enterLeaveTimeout_) {
+      clearTimeout(this.enterLeaveTimeout_);
+      delete this.enterLeaveTimeout_;
+    }
+    this.setState({ isActive: true });
+  };
+
+  onDragLeave = ev => {
+    ev.preventDefault();
+    if (this.enterLeaveTimeout_) {
+      clearTimeout(this.enterLeaveTimeout_);
+    }
+    this.enterLeaveTimeout_ = setTimeout(() => {
+      this.setState({ isActive: false });
+    }, 10);
+  };
+
+  onDragOver = ev => {
+    ev.preventDefault();
+    if (this.enterLeaveTimeout_) {
+      clearTimeout(this.enterLeaveTimeout_);
+      delete this.enterLeaveTimeout_;
+    }
+    ev.dataTransfer.dropEffect = "copy";
+  };
+
+  onDrop = ev => {
+    ev.stopPropagation();
+    ev.preventDefault();
+    if (this.props.disabled) {
+      return;
+    }
+
+    let fileNameRegexp = this.props.regex || /.*/;
+
+    this.setState({ isActive: false });
+    if (
+      ev.dataTransfer.files.length == 1 &&
+      ev.dataTransfer.files[0].name.match(fileNameRegexp)
+    ) {
+      this.props.onDrop(ev.dataTransfer.files[0]);
+    } else {
+      alert(this.props.invalidFileMessage);
+    }
+  };
+
+  render() {
+    return (
+      <div
+        className={cn(this.props.className, "file-drop-target", {
+          "is-active": this.state.isActive
+        })}
+        onDragEnter={this.onDragEnter}
+        onDragLeave={this.onDragLeave}
+        onDragOver={this.onDragOver}
+        onDrop={this.onDrop}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
