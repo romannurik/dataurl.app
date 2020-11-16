0696bcd6e8614b52a326b0f0263682d15b2bc9b1
Roman Nurik <roman@nurik.net>
2020-11-16 13:15:11 -0500

    Add analytics

---
+++ src/firebase-root.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+import 'firebase/analytics';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCp8fs-nMOQ-rQInNgc68mU8wXxGOrkDHg",
+  authDomain: "dataurl-app.firebaseapp.com",
+  databaseURL: "https://dataurl-app.firebaseio.com",
+  projectId: "dataurl-app",
+  storageBucket: "dataurl-app.appspot.com",
+  messagingSenderId: "280256532133",
+  appId: "1:280256532133:web:0263cee39eb645d1b2474c",
+  measurementId: "G-ZLL0WMG7FK"
+});
+
+firebase.analytics();
